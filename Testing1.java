1b112dceab4564a2804e0ea1660eb0d85c7ac324
William Ambrose <W.P.Ambrose@gmail.com>
2015-07-11 23:32:37 -0400

    Trying some Java

---
+++ Testing1.java
@@ -0,0 +1,14 @@
+public class Testing1
+{
+	public static void main (String[] args)
+	{
+		String a = "Good evening, universe.";
+		int b;
+		
+		System.out.print(a + "\n\n");
+		for (b = 0; b <= 10; b++)
+		{
+			System.out.println(b);
+		}
+	}
+}
