d27b8125252eb54d6fa7be81068de3d531f78ab7
William Ambrose <W.P.Ambrose@gmail.com>
2015-07-21 14:29:46 -0400

    Moving forward, trying more simple things

--- Testing1.java
+++ Testing1.java
@@ -6,9 +6,14 @@ public class Testing1
 		int b;
 		
 		System.out.print(a + "\n\n");
+		
 		for (b = 0; b <= 10; b++)
 		{
 			System.out.println(b);
 		}
+		
+		a = System.console().readLine();
+		
+		System.out.println(a);
 	}
 }
